f3a6ba528cb362f371371ac0d839959ffb8020ed
sharior13 <sauravshrestha648@gmail.com>
2025-12-20 17:58:41 +0545

    Update

--- entities.js
+++ entities.js
@@ -1,7 +1,10 @@
 class Entity {
-    constructor({position, velocity}){
+    constructor({position}){
         this.position = position;
-        this.velocity = velocity;
+        this.direction = {
+            x: 1,
+            y: 1
+        }
     }
 
     get left () {
@@ -27,23 +30,35 @@ class Entity {
 
 
 class Duck extends Entity {
-    constructor({position, velocity, color}){
-        super({position, velocity});
+    constructor({position}){
+        super({position});
         this.size = {
             width: 100,
             height: 100
-        },
-        this.color = color;
+        }
+        this.velocity = {
+            x: 1,
+            y: 1
+        };
+        this.direction = {
+            x: (Math.round(Math.random()) *2) - 1,
+            y: (Math.round(Math.random()) *2) - 1
+        };
+        this.color = "orange";
     }
 
     move() {
-        this.position.x += this.velocity.x;
+        this.position.x += this.velocity.x*this.direction.x;
     }
 }
 
 class Egg extends Entity {
-    constructor({position, velocity}){
-        super({position, velocity});
+    constructor({position, direction, original}){
+        super({position, direction});
+        this.velocity = {
+            x: 1,
+            y: 1
+        };
         this.radius = {
             x: 30,
             y: 50
@@ -54,39 +69,66 @@ class Egg extends Entity {
             end: Math.PI*2
         };
         this.color = "gray";
+        this.original = original;
+        this.isCollected = false;
     }
 
     draw(ctx) {
+        if(this.isCollected){
+            this.position.x = this.original.x;
+            this.position.y = this.original.y;
+            this.isCollected = false; 
+            return;
+        }
         ctx.beginPath();
-        ctx.ellipse(this.position.x,this.position.y, this.radius.x, this.radius.y, this.rotation, this.angle.start, this.angle.end);
+        ctx.ellipse(this.position.x, this.position.y, this.radius.x, this.radius.y, this.rotation, this.angle.start, this.angle.end);
         ctx.fillStyle = this.color;
         ctx.fill();
         ctx.closePath();
     }
+
+    move() {
+        this.position.y += this.velocity.y;
+    }
 }
 
 class Player extends Entity{
-    constructor({position, velocity}){
-        super({position, velocity});
+    constructor({position, direction}){
+        super({position, direction});
         this.size = {
             width: 100,
             height: 175
         };
+        this.velocity = {
+            x: 1,
+            y: 1
+        };
         this.color = "red";
     }
 
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.position.x,this.position.y,this.size.width,this.size.height);
+        ctx.closePath();
+    }
+
     move(key) {
-        // switch(key){
-        //     case "d":{
-        //         this.position.x+=this.velocity.x;
-        //         break;   
-        //     }
-        //     case "a":{
-        //         this.position.x-=this.velocity.x;
-        //         break;
-        //     }
-        // }
+        switch(key){
+            case "d":{
+                this.position.x+=this.velocity.x;
+                break;   
+            }
+            case "a":{
+                this.position.x-=this.velocity.x;
+                break;
+            }
+        }
     }
 }
 
 export {Duck, Egg, Player};
+
+//015970488
+
+//1660-01-07777
--- main.js
+++ main.js
@@ -15,18 +15,25 @@ canvas.height = window.innerHeight;
 let ctx = canvas.getContext("2d");
 
 let hasStarted = false;
+let score = 0;
+const duck = [];
+const egg = [];
 const gameStart = () => {
-        animate();
-        console.log("game STARTTTT");
+    console.log("STARTTTT");
+        hasStarted = true;
         window.removeEventListener("keydown", gameStart)
+        animate();
 };
-
-if(!hasStarted){
-    hasStarted = true;
+const titleScreen = ()=>{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
     ctx.font = "50px Arial";
+    ctx.fillStyle = "black";
     ctx.fillText("Press any key to start", canvas.width/2-250, canvas.height/2, 500);
+    ctx.closePath();
     window.addEventListener("keydown", gameStart);
 }
+titleScreen();
 
 
 const player = new Player({
@@ -35,54 +42,76 @@ const player = new Player({
         y: 475
     }
 });
+for(let i=0; i<4; i++){
+    duck.push(new Duck({
+        position:{
+            x: 100+i*400,
+            y: 10
+        },
+        direction:{
+            x: (Math.round(Math.random()) *2) - 1,
+            y: (Math.round(Math.random()) *2) - 1
+        }
+    }));
+    egg.push(new Egg({
+        position:{
+            x: duck[i].position.x+50,
+            y: duck[i].position.y+150
+        },
+        original:{   
+            x: duck[i].position.x+50,
+            y: duck[i].position.y+150
+        }
+    }));
+}
 
-const duck1 = new Duck({
-    position:{
-        x: 50,
-        y: 10
-    },
-    velocity:{
-        x: 1,
-        y: 1
-    },
-    color: "orange"
-});
-const duck2 = new Duck({
-    position:{
-        x: 200,
-        y: 10
-    },
-    velocity:{
-        x: 0.5,
-        y: 1
-    },
-    color: "red"
-});
-
-const egg = new Egg({
-    position:{
-        x: 100,
-        y: 200
-    }
-});
 
 const animate = ()=>{
+    if(!hasStarted){
+        return;
+    }
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     line(0, 110, canvas.width, 110);
     line(0, 650, canvas.width, 650);
     player.draw(ctx);
-    duck2.draw(ctx);
-    duck1.draw(ctx);
-    egg.draw(ctx);
-    duck2.move();
-    duck1.move();
-    // window.addEventListener("keydown",(event)=>{
-    //     player.move(event.key);
-    // });
 
-    if(doesCollide(duck1,duck2)){
-        console.log("quack");
+    for(let i=0; i<duck.length; i++){
+        for(let j=0; j<duck.length; j++){
+            if(duck[i].right>=duck[j].left && duck[i].left<=duck[j].right){
+                duck[i].direction.x *= -1;
+                duck[j].direction.x *= -1;
+            }
+        }
+        if(duck[i].right>=canvas.width || duck[i].left<= 0){
+            duck[i].direction.x *= -1;
+        }
+
+        if(player.top <= egg[i].position.y+egg[i].radius.y && player.bottom >= egg[i].position.y+egg[i].radius.y && player.right>= egg[i].position.x-egg[i].radius.x && player.left <= egg[i].position.x+egg[i].radius.x){
+            egg[i].isCollected = true;
+            score++;
+        }
+        duck[i].draw(ctx);
+        duck[i].move();
+        egg[i].original = {
+            x: duck[i].position.x+50,
+            y: duck[i].position.y+150
+        }
+        egg[i].draw(ctx);
+        egg[i].move();
+
+        // if(egg[i].position.y-egg[i].radius.y > canvas.height){
+        //         egg[i].position = {
+        //         x: egg[i].original.x,
+        //         y: egg[i].original.y
+        //     }
+        // }
     }
+    
+    window.addEventListener("keydown",(event)=>{
+        player.move(event.key);
+    });
+    trackScore();
+    gameEnd();
     requestAnimationFrame(animate);
 };
 
@@ -94,8 +123,34 @@ const line = (x1, y1, x2, y2)=>{
     ctx.closePath(); 
 };
 
-const doesCollide = (box1,box2)=>{
-    let collideX = (box1.right >= box2.left && box2.right >= box1.left);
-    let collideY = (box1.bottom >= box2.top && box2.bottom >= box1.top);
-    return collideX && collideY;
+const trackScore = () =>{
+    ctx.beginPath();
+    ctx.font = "50px Arial";
+    ctx.fillStyle = "red";
+    ctx.fillText(`Score: ${score}`, canvas.width/2-100, canvas.height-5, 200);
+    ctx.closePath();
 };
+
+const gameEnd = () =>{
+        if(egg.every((anda)=>{
+        return anda.position.y-anda.radius.y > canvas.height;
+    }))
+    {
+        hasStarted = false;
+        ctx.beginPath();
+        ctx.font = "50px Arial";
+        ctx.fillStyle = "red";
+        ctx.fillText("Game Over.", canvas.width/2-150, canvas.height/2, 300);
+        ctx.closePath();
+        egg.forEach(anda=>{
+            anda.position = {
+                x: anda.original.x,
+                y: anda.original.y
+            }
+        });
+        score = 0;
+        setTimeout(()=>{
+            titleScreen();
+        },2000);
+    }
+}
